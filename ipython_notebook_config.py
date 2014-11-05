1a3a6021f3add739dd74c95d2f64f2d149bdf7cd
Kyle Kelley <rgbkrk@gmail.com>
2014-11-05 11:10:35 +0000

    Take out CSP, fix up x-frame-options

--- ipython_notebook_config.py
+++ ipython_notebook_config.py
@@ -14,8 +14,7 @@ c.NotebookApp.trust_xheaders = True
 # uses.
 c.NotebookApp.webapp_settings = {
     'headers': {
-        'X-Frame-Options': 'ALLOW FROM nature.com',
-        'Content-Security-Policy': "frame-ancestors 'self' *.nature.com"
+        'X-Frame-Options': 'ALLOW FROM http://www.nature.com'
     },
     'template_path':['/srv/ga/', '/srv/ipython/IPython/html',
                      '/srv/ipython/IPython/html/templates']
